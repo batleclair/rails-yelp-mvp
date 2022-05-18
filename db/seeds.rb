544bae21c66c891547c6ea1c99714ba0fb4d7827
Baptiste Clair <baptisteclair@gmail.com>
2022-05-18 14:18:32 +0200

    restaurant reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+  categories = %w[chinese italian japanese french belgian]
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.cell_phone_in_e164
+  category = categories[rand(0..categories.count)]
+  restaurant = Restaurant.new(name: name, address: address, phone_number: phone_number, category: category)
+  restaurant.save
+end
